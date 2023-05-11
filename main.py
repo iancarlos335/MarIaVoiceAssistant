9c46876d4febc98f985a846280a98d93f5e8a2b2
ian-carlos <iancarlos335@gmail.com>
2023-05-11 14:04:27 -0300

    Colocando as bibliotecas corretamente

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-# This is a sample Python script.
-import openai
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import asyncio
+from EdgeGPT import Chatbot, ConversationStyle
+
+async def main():
+    bot = await Chatbot.create(cookie_path='cookies.json')
+    print(await bot.ask(prompt="Hello world", conversation_style=ConversationStyle.creative, wss_link="wss://sydney.bing.com/sydney/ChatHub"))
+    await bot.close()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    asyncio.run(main())
