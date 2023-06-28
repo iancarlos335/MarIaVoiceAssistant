cde96c104c17370b7f091b8fdb0c0d6dd1d74385
ian-carlos <iancarlos335@gmail.com>
2023-06-27 22:40:04 -0300

    Printando conteúdo presente numa requesição

--- MariaAPI/backend/api/views.py
+++ MariaAPI/backend/api/views.py
@@ -1,5 +1,19 @@
+import json
 from django.http import JsonResponse
 
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hi there, this is Ian API response"})
+    print(request.GET)  # url query params
+    print(request.POST)
+    body = request.body  # byte string with JSON data
+    data = {}
+    try:
+        data = json.loads(body)  # string of JSON data-> Python Dict
+    except:
+        pass
+    print(data)
+    json.dumps(dict(request.headers))
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
--- MariaAPI/py_client/basic.py
+++ MariaAPI/py_client/basic.py
@@ -4,8 +4,8 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, json={"query":"Hello World!"})
-print(get_response.text)
-print(get_response.status_code)
+get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello World!"})
+# print(get_response.text)
+# print(get_response.status_code)
 
-print(get_response.json()['message'])
+# print(get_response.json()['message'])
