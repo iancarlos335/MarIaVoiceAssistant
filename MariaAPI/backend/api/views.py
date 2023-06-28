92ba497b8b9759cbe899b8d6a3a39a584f1f02c0
ian-carlos <iancarlos335@gmail.com>
2023-06-28 13:06:34 -0300

    Começando a entender a serializa~ção dos dois produtos criados pelo shell no ambiente e salvos em cache provavelmente

--- MariaAPI/backend/api/views.py
+++ MariaAPI/backend/api/views.py
@@ -1,19 +1,17 @@
 import json
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from django.forms.models import model_to_dict
+
+from products.models import Product
 
 
 def api_home(request, *args, **kwargs):
-    print(request.GET)  # url query params
-    print(request.POST)
-    body = request.body  # byte string with JSON data
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(body)  # string of JSON data-> Python Dict
-    except:
-        pass
-    print(data)
-    json.dumps(dict(request.headers))
-    data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
+    if model_data:
+        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
     return JsonResponse(data)
+    #     print(data)
+    #     data = dict(data)
+    #     json_data_str = json.dumps(data)
+    # return HttpResponse(json_data_str, headers={"content-type":"application/json"})
--- MariaAPI/py_client/basic.py
+++ MariaAPI/py_client/basic.py
@@ -4,8 +4,11 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello World!"})
-# print(get_response.text)
+get_response = requests.get(endpoint, params={"product_id": 123})
+# print(get_response.text) # prints raw text response
 # print(get_response.status_code)
 
-# print(get_response.json()['message'])
+# HTTP Request -> HTML
+# REST API HTTP Request -> JSON
+# JavaScript Object Notation ~ Python Dict
+print(get_response.json()) #
