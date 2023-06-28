e4839e2a323dcc073a00feca3e5cba52ce7ce494
ian-carlos <iancarlos335@gmail.com>
2023-06-27 22:09:38 -0300

    Meu primeiro controller com uma APIdjango

--- MariaAPI/py_client/basic.py
+++ MariaAPI/py_client/basic.py
@@ -1,7 +1,11 @@
 import requests
 
-endpoint = "https://httpbing.org/status/200/"
-endpoint = "https://httpbing.org/"
+# endpoint = "https://httpbin.org/status/200/"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint)
+get_response = requests.get(endpoint, json={"query":"Hello World!"})
 print(get_response.text)
+print(get_response.status_code)
+
+print(get_response.json()['message'])
