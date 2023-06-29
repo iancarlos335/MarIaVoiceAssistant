ec1fb0f1cfcaf7561ca18ad00805e151c1f63173
ian-carlos <iancarlos335@gmail.com>
2023-06-28 22:44:55 -0300

    Relizando a primeira request Serializada para JSON com dados diferentes de string padrão

--- MariaAPI/backend/api/views.py
+++ MariaAPI/backend/api/views.py
@@ -1,17 +1,18 @@
-import json
-from django.http import JsonResponse, HttpResponse
-from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from products.models import Product
+from products.serializers import ProductSerializer
 
 
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by("?").first()
+    """
+    DRF API View
+    """
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    if model_data:
-        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
-    return JsonResponse(data)
-    #     print(data)
-    #     data = dict(data)
-    #     json_data_str = json.dumps(data)
-    # return HttpResponse(json_data_str, headers={"content-type":"application/json"})
+    if instance:
+        # data = model_to_dict(instance, fields=['id', 'title', 'price', 'sale_price'])
+        data = ProductSerializer(instance).data
+    return Response(data)
