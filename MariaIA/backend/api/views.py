89058624f13039e08ea93669d56860d3a9838487
Ian Carlos Azevedo Martins <iancarlos335@gmail.com>
2024-02-11 20:57:49 -0300

    Modifing gitignore

--- MariaIA/backend/api/views.py
+++ MariaIA/backend/api/views.py
@@ -9,7 +9,7 @@ def api_home(request, *args, **kwargs):
         data = json.loads(body)
     except:
         pass
-    #data['headers'] = request.headers
-    data['content_type'] = request.content_type
+    data['content_type'] = request.content_type    
     #data['headers'] = dict(request.headers)
+    
     return JsonResponse(data)
--- MariaIA/py_client/basic.py
+++ MariaIA/py_client/basic.py
@@ -2,5 +2,5 @@ import requests
 
 endpoint = "http://127.0.0.1:8000/api"
 
-get_response = requests.get(endpoint, params={"abc": 123}, json={"test": "hello world"})
+get_response = requests.get(endpoint, json={"test": "hello world"})
 print(get_response.json())
